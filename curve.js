fa5f274aa072c44f985f0acd685d0ebcf958512b
Patricio Marrone <marrone.patricio@gmail.com>
2017-01-21 16:48:12 -0300

    Wave controls kind of working

--- curve.js
+++ curve.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var Curve = {
-  step: 1,
+  step: (Math.PI * 2) / 360,
   freq: 3,
   amp: 1,
   phase: 4,
@@ -15,7 +15,7 @@ var Curve = {
   yAxis: Math.floor(jsGFwk.settings.width / 4),
   freqIncrement: 0.1,
   ampIncrement: 0.1,
-  offset: 4,
+  offset: 2,
   init: function () {
 
   },
@@ -24,7 +24,7 @@ var Curve = {
     //   this.freqIncrement = -this.freqIncrement;
     // }
     //this.setFrequency(this.freq + this.freqIncrement);
-    this.t = this.t + delta * 1;
+    this.t = (this.t + delta * 1);
 
     this.handleInput(delta);
 
@@ -45,6 +45,14 @@ var Curve = {
     if (jsGFwk.IO.keyboard.getActiveKeys()[jsGFwk.IO.keyboard.key.S]) {
       this.amp -= this.ampIncrement;
     }
+
+    if (jsGFwk.IO.keyboard.getActiveKeys()[jsGFwk.IO.keyboard.key.N]) {
+      this.t += this.freqIncrement;
+    }
+
+    if (jsGFwk.IO.keyboard.getActiveKeys()[jsGFwk.IO.keyboard.key.M]) {
+      this.t -= this.freqIncrement;
+    }
   },
 
   setFrequency: function(newFrequency) {
@@ -55,18 +63,40 @@ var Curve = {
     this.phase = (this.t + this.offset) * (oldFreq - this.freq) + oldPhase;
   },
   draw: function (ctx) {
-    var x = this.t;
-    var y = Math.sin(x * this.freq + this.phase) * this.amp;
+    var x = 0;
+    var y = this.getHeight(x, this.t, this.freq, this.phase, this.amp);
+
 
     ctx.beginPath();
-    ctx.moveTo(this.yAxis, this.unit * y + this.xAxis);
+    ctx.moveTo(x * this.unit + this.yAxis, this.unit * y + this.xAxis);
     // Loop to draw segments
 
-    for (var i = this.yAxis; i <= this.width; i += this.step) {
-      x = this.t + (-this.yAxis + i) / this.unit;
-      y = Math.sin(x * this.freq + this.phase) * this.amp;
-      ctx.lineTo(i, this.unit * y + this.xAxis);
+    for (x = 0; x <= this.width; x += this.step) {
+      y = this.getHeight(x, this.t, this.freq, this.phase, this.amp);
+      ctx.lineTo(x * this.unit + this.yAxis, this.unit * y + this.xAxis);
     }
     ctx.stroke();
+
+    this.drawCircle(ctx);
+
+    ctx.fillStyle = "black";
+    ctx.font = "10pt arial";
+    ctx.fillText("Freq: " + this.freq + ", Amp: " + this.amp + ", Offset: " + this.offset + ", T: " + this.t, 10, 20);
+
+  },
+  drawCircle: function (ctx) {
+    var x = this.offset;
+    var y = this.getHeight(x, this.t, this.freq, this.phase, this.amp);
+    //var y = Math.sin(this.t + this.offset * this.freq + this.phase) * this.amp * this.unit;
+    ctx.beginPath();
+    //ctx.moveTo(x, y);
+
+    ctx.arc(x * this.unit + this.yAxis, this.unit * y + this.xAxis, 10, 0, 2*Math.PI, false);
+    ctx.stroke();
+  },
+  getHeight: function (x, t, freq, phase, amp) {
+    return Math.sin((x + t) * freq + phase) * amp;
   }
+
+
 };
